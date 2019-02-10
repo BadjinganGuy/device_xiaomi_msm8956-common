0db0ab3ee50a96d6bea06f6adeef80ea0070845f
Ангга Роми Сапутра <help@h1t.me>
2019-02-10 04:20:35 +0300

    kenzo: Init Dirac

--- msm8956.mk
+++ msm8956.mk
@@ -180,6 +180,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1-service.clearkey
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Face detection extension
 PRODUCT_PACKAGES += \
     org.codeaurora.camera
